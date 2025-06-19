6d0584a0136f7d098b3e8c0c7f096ef98458e797
sunflowerthomas <t.man.sunflower@gmail.com>
2025-06-19 20:16:52 +0100

    added basic things

---
+++ AI_Text_Adventure.py
@@ -0,0 +1,7 @@
+import http
+
+aiurl = "ai.hackclub.com/chat/completions"
+
+print("What would you like the initial prompt to be?")
+prompt = input("Enter here: ")
+print(prompt)
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # AI Text Adventure
+Basically, a text adventure that is generated on-the-fly from a single starter prompt using AI.
